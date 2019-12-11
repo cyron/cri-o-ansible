fe96630263191a77f5a9409b8fd5f846ca6391dd
cyron chen <asyura.csr@gmail.com>
2019-12-11 14:37:37 +0900

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,12 @@
 # Git proxy setting
 git config --global http.proxy http://proxy.rdh.ecl.ntt.co.jp:20066
 git config --global https.proxy http://proxy.rdh.ecl.ntt.co.jp:20066
+
+yum install epel-release -y
+yum update -y
+yum install ansible -y
+
+git clone https://github.com/cyron/cri-o-ansible
+cd cri-o-ansible
+git checkout fixes_k8s_1_16
+ansible-playbook cri-o.yml -i 10.0.2.11,
