640dbd12359cfe3e49f77183113fcb4800803538
cyron chen <asyura.csr@gmail.com>
2019-12-12 09:30:36 +0900

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,10 +2,6 @@
 ssh-keygen
 ssh-copy-id 10.0.2.11
 
-# Git proxy setting
-git config --global http.proxy http://proxy.rdh.ecl.ntt.co.jp:20066
-git config --global https.proxy http://proxy.rdh.ecl.ntt.co.jp:20066
-
 yum install epel-release -y
 yum update -y
 yum install ansible -y
